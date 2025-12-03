df5cf67ab2790d7b87b096da35f2ccf6799e24fc
hudagraph <teachgen2025@gmail.com>
2025-12-03 11:39:28 +0700

    update astro.config dan layout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // Ganti ini dengan domain aslimu nanti kalau sudah upload
-  // Contoh: 'https://beasiswayes.id'
-  site: 'https://contoh-website-kamu.netlify.app', 
+  site: 'https://yes-landingpage.netlify.app',
   
   integrations: [tailwind()],
 });
