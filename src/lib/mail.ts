1372180cf4152174e005b0c239e210144b01440d
teachd3v <teach.dev@gmail.com>
2026-04-30 16:11:41 +0700

    feat: implement confirmation email service using Resend for application submissions

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -2,7 +2,7 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-interface EmailDocData {
+export interface EmailDocData {
   biodata: {
     nama: string;
     nik: string;
